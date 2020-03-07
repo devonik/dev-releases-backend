f58ee93e7d6e360d0b247424613598970d3fb2b7
Niklas Grieger <niklas.grieger1@gmail.com>
2020-03-07 19:11:12 +0100

    Revert "init"
    This reverts commit bb3fb839bf2633d52de01dbda2251bb2e12d5dc5.

--- resources/views/vendor/backpack/crud/buttons/tech_latest_release.blade.php
+++
@@ -1,3 +0,0 @@
-@if ($crud->hasAccess('update'))
-    <a href="{{ url($crud->route.'/'.$entry->getKey().'/getLatestRelease') }} " class="btn btn-xs btn-default"><i class="fa fa-wifi"></i> Get latest release</a>
-@endif
