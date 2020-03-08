d150b1e873b8cc736770082cbbd5cc8c03147a99
Niklas Grieger <niklas.grieger1@gmail.com>
2020-03-08 21:09:57 +0100

    schedule task to check new releases

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        //$schedule->call(new CheckReleases)->dailyAt('10:00');
+        $schedule->call(new CheckReleases)->everyMinute();
     }
 
     /**
