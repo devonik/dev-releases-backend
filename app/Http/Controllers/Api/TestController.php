e5e572709bcb65d8fa824906b596a93fffd40bf3
Niklas Grieger <niklas.grieger1@gmail.com>
2020-03-14 00:22:50 +0100

    send real notification data

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -20,12 +20,16 @@ class TestController
             $tech = $tech[0];
 
             //Lets send firebase push notifications
+            $topic = 'new-tech-release';
             $title = 'New github release';
             $body = $tech->title.' released to '.$tech->latest_tag;
             $notification = ['title' => $title, 'body' => $body];
             $notificationData = $tech->toArray();
+            //Needed so we can read the send message if the app is in background
             $notificationData['click_action'] = 'FLUTTER_NOTIFICATION_CLICK';
-            $firebaseMessage = CloudMessage::withTarget('topic', 'new-tech-release')
+            //Needed because the notification itself is not attatched on background message
+            $notificationData['message_identifier'] = $topic;
+            $firebaseMessage = CloudMessage::withTarget('topic', $topic)
                 ->withNotification($notification)
                 ->withData($notificationData);
 
