e263a10fd47b1ed975cbdb075fffc4fe6c0ef875
Niklas Grieger <niklas.grieger1@gmail.com>
2020-03-17 21:09:11 +0100

    catch empty release case

--- app/Repositories/TechRepository.php
+++ app/Repositories/TechRepository.php
@@ -65,65 +65,6 @@ class TechRepository
         }
         return $githubReponse;
 
-
-        /*$latestRelease = Http::get("https://api.github.com/repos/$tech->github_owner/$tech->github_repo/releases/latest");
-        //TODO some repos doesn't have releases (Example flutter https://github.com/flutter/flutter/releases) so we have to grab tags and take latest
-        if($latestRelease->ok()){
-            //If the Https Status is 200 (ok)
-            if($latestRelease->json()['tag_name'] != $tech->latest_tag){
-                //If the tag is new lets update the database entry
-                $tech->update(
-                        [
-                            "latest_tag" => $latestRelease->json()['tag_name'],
-                            "github_release_id" =>  $latestRelease->json()['id'],
-                            "github_link" => $latestRelease->json()['html_url'],
-                            "release_published_at" => $latestRelease->json()['published_at'],
-                            "body" => $latestRelease->json()['body']
-                        ]
-                    );
-                return $tech;
-            }
-
-        }else{
-            //The response is not ok in following scenarios
-            //1. The repo does'nt have a latest release so we get "Method not found"
-
-            //Case 1: We have to call the tags api and take the latest to update our database entry
-            $tags = Http::get("https://api.github.com/repos/$tech->github_owner/$tech->github_repo/tags");
-            if($tags->ok()){
-                //If the Https Status is 200 (ok)
-                $latestTagObject = $tags->json()[0];
-                if($latestTagObject['name'] != $tech->latest_tag) {
-                    //If the tag is new lets update the database entry
-                    $responseCommit = Http::get($latestTagObject['commit']['url']);
-
-                    $githubLink = 'https://github.com/'.$tech->github_owner.'/'.$tech->github_repo.'/';
-                    $githubPublishedAt = Carbon::now();
-                    $body = 'No body text found';
-                    if($responseCommit->ok()){
-                        //If we can get the commit data lets take it
-                        $githubPublishedAt = $responseCommit->json()['commit']['author']['date'];
-                        $githubLink = $responseCommit->json()['html_url'];
-                        $body = $responseCommit->json()['commit']['message'];
-                    }
-                    //If the Https Status is 200 (ok) lets update our database
-                    $tech->update(
-                            [
-                                "latest_tag" => $latestTagObject['name'],
-                                "github_link" => $githubLink,
-                                "release_published_at" => $githubPublishedAt,
-                                "body" => $body
-                            ]
-                        );
-
-                    return $tech;
-                }
-
-            }
-
-        }
-*/
-        //return false;
     }
 
     /**
@@ -155,24 +96,31 @@ class TechRepository
             //Case 1: We have to call the tags api and take the latest to update our database entry
             $tags = Http::get("https://api.github.com/repos/$owner/$repo/tags");
             if ($tags->ok()) {
-                $latestTag = isset($tags->json()[0]) ? $tags->json()[0] : null;
-                if($latestTag){
-                    //If the tag is new lets update the database entry
-                    $responseCommit = Http::get($latestTag['commit']['url']);
+                if(count($tags->json()) === 0){
+                    //The Call can be OK but the tags can be empty - lets grab this
+                    $response['error'] = 'There is no release for this repo';
+                    $response['detailError'] = $tags->json();
+                }else{
+                    $latestTag = isset($tags->json()[0]) ? $tags->json()[0] : null;
+                    if($latestTag){
+                        //If the tag is new lets update the database entry
+                        $responseCommit = Http::get($latestTag['commit']['url']);
 
-                    $response['tech']->latest_tag = $latestTag['name'];
-                    $response['tech']->github_link = 'https://github.com/'.$owner.'/'.$repo.'/';
-                    $response['tech']->release_published_at = Carbon::now();
-                    $response['tech']->body = 'No body text found';
-                    if($responseCommit->ok()){
-                        //If we can get the commit data lets take it
-                        $response['tech']->github_link = $responseCommit->json()['html_url'];
-                        $response['tech']->release_published_at = $responseCommit->json()['commit']['author']['date'];
-                        $response['tech']->body = $responseCommit->json()['commit']['message'];
+                        $response['tech']->latest_tag = $latestTag['name'];
+                        $response['tech']->github_link = 'https://github.com/'.$owner.'/'.$repo.'/';
+                        $response['tech']->release_published_at = Carbon::now();
+                        $response['tech']->body = 'No body text found';
+                        if($responseCommit->ok()){
+                            //If we can get the commit data lets take it
+                            $response['tech']->github_link = $responseCommit->json()['html_url'];
+                            $response['tech']->release_published_at = $responseCommit->json()['commit']['author']['date'];
+                            $response['tech']->body = $responseCommit->json()['commit']['message'];
+                        }
                     }
                 }
+
             }else{
-                $response['error'] = 'Could not get github data';
+                $response['error'] = 'Could not get any github data';
                 $response['detailError'] = $tags->json();
             }
         }
